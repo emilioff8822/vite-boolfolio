db958d228f621f79dfc64031e81053017328d86b
emilioff8822 <emilioff@hotmail.it>
2023-06-26 22:26:10 +0200

    cards api

--- front/src/App.vue
+++ front/src/App.vue
@@ -44,25 +44,30 @@ export default {
   <div class="container">
     <h1>Elenco Post</h1>
 
-    <ul>
-      <li v-for="post in posts" :key="post.id">
-        <span>{{ post.title }}</span> - <span>{{ formatData(post.date) }}</span>-
-        <strong>{{ post.category.name }}</strong>-
-        <strong v-for="tag in post.tags" key="tag.id">{{ tag.name }}</strong>
-
-      </li>
-    </ul>
-
-    <div>
-      <button @click="getApi(first_page_url)" :disabled="current_page == 1">Inizio</button>
-      <button v-for="(link, index) in links" key="index" v-html="link.label" @click="getApi(link.url)"
-        :disabled="link.active || !link.url"></button>
-      <button @click="getApi(last_page_url)" :disabled="current_page == last_page">Fine</button>
-
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-lg-4 mb-4" v-for="post in posts" :key="post.id">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ post.title }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ formatData(post.date) }}</h6>
+            <p class="card-text">{{ post.category.name }}</p>
+            <p class="card-text" v-for="tag in post.tags" :key="tag.id">{{ tag.name }}</p>
+          </div>
+        </div>
+      </div>
     </div>
 
+    <div class="d-flex justify-content-between">
+      <button class="btn btn-primary" @click="getApi(first_page_url)" :disabled="current_page == 1">Inizio</button>
+      <div v-for="(link, index) in links" :key="index">
+        <button class="btn btn-secondary mx-1" v-html="link.label" @click="getApi(link.url)"
+          :disabled="link.active || !link.url"></button>
+      </div>
+      <button class="btn btn-primary" @click="getApi(last_page_url)" :disabled="current_page == last_page">Fine</button>
+    </div>
   </div>
 </template>
+
 
 <style lang="scss">
 
