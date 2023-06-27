723a0ba63d8c5c1d449dc33d54f6a602febf2888
emilioff8822 <emilioff@hotmail.it>
2023-06-27 21:44:05 +0200

    api categories list

--- front/src/pages/Blog.vue
+++ front/src/pages/Blog.vue
@@ -1,4 +1,3 @@
-
 <script>
 import { store } from '../store/store';
 import axios from 'axios';
@@ -8,7 +7,6 @@ export default {
     name: 'Contacts',
     components: {
         ItemPost
-
     },
     data() {
         return {
@@ -17,23 +15,35 @@ export default {
             first_page_url: null,
             last_page_url: null,
             current_page: null,
-            last_page: null
+            last_page: null,
+            categories: []
         };
     },
     methods: {
         getApi(endpoint) {
-            console.log(store.apiUrl);
-            //faccio la chiamata axios
             axios.get(endpoint)
                 .then(results => {
-                    this.posts = results.data.data
-                    this.links = results.data.links
-                    this.first_page_url = results.data.first_page_url
-                    this.last_page_url = results.data.last_page_url
-                    this.current_page = results.data.current_page
-                    this.last_page = results.data.last_page
+                    this.posts = results.data.data;
+                    this.links = results.data.links;
+                    this.first_page_url = results.data.first_page_url;
+                    this.last_page_url = results.data.last_page_url;
+                    this.current_page = results.data.current_page;
+                    this.last_page = results.data.last_page;
                 });
         },
+
+        // nei methods aggiungo getcategories dell;' API -PostController, l'apiurl e' store.js e gli concateno categories
+
+        getCategories() {
+            axios.get(store.apiUrl + 'posts/categories')
+                .then(result => {
+                    this.categories = result.data;
+                })
+
+        },
+
+
+
         formatData(dateString) {
             const d = new Date(dateString);
             return d.toLocaleDateString();
@@ -41,32 +51,54 @@ export default {
     },
     mounted() {
         this.getApi(store.apiUrl + 'posts');
+        this.getCategories();
     }
-
-
 }
 </script>
-
 
 <template>
     <div class="container-inner">
         <h1>Blog</h1>
 
-        <div>
-            <ItemPost v-for="post in posts" :key="post.id" :post="post" />
+        <div class="page-wrapper">
+            <div class="left">
+                <ItemPost v-for="post in posts" :key="post.id" :post="post" />
+
+                <button @click="getApi(first_page_url)" :disabled="current_page == 1">
+                    Inizio
+                </button>
+
+                <button v-for="(link, index) in links" :key="index" v-html="link.label" @click="getApi(link.url)"
+                    :disabled="link.active || !link.url" />
+
+                <button @click="getApi(last_page_url)" :disabled="current_page == last_page">
+                    Fine
+                </button>
+            </div>
+
+            <div class="right">
+                <h2>Categorie</h2>
+                <button v-for="category in categories " :key="category.id">{{ category.name }}</button>
+
+            </div>
 
         </div>
 
 
-
-    </div>
-
-    <div>
-        <button @click="getApi(first_page_url)" :disabled="current_page == 1">Inizio</button>
-        <button v-for="(link, index) in links" key="index" v-html="link.label" @click="getApi(link.url)"
-            :disabled="link.active || !link.url"></button>
-        <button @click="getApi(last_page_url)" :disabled="current_page == last_page">Fine</button>
-
     </div>
 </template>
-<style></style>
+
+<style lang="scss" scoped>
+.page-wrapper {
+    display: flex;
+
+    .left {
+        width: 100%;
+    }
+
+    .right {
+        border-left: 1px solid black;
+        padding: 0 30px;
+    }
+}
+</style>
